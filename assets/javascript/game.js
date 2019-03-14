e30973a6154ce26d92ea8d68cf406bb6d5514da1
Kunal <patelkr07@gmail.com>
2019-03-13 22:50:58 -0500

    win counter still not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,86 @@
-document.onload = function () { }
+window.onload = function () { 
 
 
     var words = ["tornado", "lightning", "hail", "wind", "hurricane", "waterspout", "flash flood", "earthquake", "volcano", "avalanche", "thunderstorm", "rain", "tsunami", "landslide", "wildfire", "sinkhole", "dust devil"]
 
     var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-    
-    var guessmax = 10
 
-    var blankspace = "_"
+    const guessMax = 10
+    var letterGuessed = []
+    var wordGuessed = []
+    var solution
+    var attempts
+    var wins = 0
 
-    var randomWord = words[Math.floor(Math.random()*words.length)];
+    resetGame()
 
-    var numBlanks = randomWord.length;
-
-    var blanks = blankspace.repeat(numBlanks);
-
-    document.write(blanks);
-
-
-    document.write(randomWord);
-
-    document.onkeyup = function(event) {
-    var userGuess = event.key;
-
-document.write(userGuess);
+    document.onkeypress = function(event) {
+        if (alphabetic(event.key)) {
+            checkForLetter(event.key.toUpperCase())
+        }
     }
+    
+    
+
+    function checkForLetter(letter) {
+        var gottem = false
+        
+        for(var i=0, j=solution.length; i<j; i++) {
+            if (letter === solution[i]) {
+                wordGuessed[i] = letter
+                gottem = true
+                if (wordGuessed.join("") === solution) { 
+                    wins++
+                }
+            }
+        }
+        
+        if(!gottem) {
+            if(!letterGuessed.includes(letter)) {
+                letterGuessed.push(letter)
+                attempts--
+            }
+
+            if (attempts === 0) {
+                wordGuessed = solution.split()                
+            }
+        }
+
+        updateDisplay()
+    }
+
+    function alphabetic (ch){
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    function resetGame() {
+        attempts = guessMax
+
+        solution = words[Math.floor(Math.random() * words.length)].toUpperCase()
+        
+        letterGuessed = []
+        wordGuessed = []
+
+        for (var i=0, j=solution.length; i < j; i++){
+            if (solution[i] === " ") {
+                wordGuessed.push(" ")
+            } else {
+                wordGuessed.push("_")
+            }
+        }
+
+
+        updateDisplay()
+    }
+    
+    function updateDisplay () {
+        document.getElementById("currentWord").innerText = wordGuessed.join("")
+        document.getElementById("winTotes").innerText = wins
+        document.getElementById("guessesLeft").innerText = attempts
+        document.getElementById("letterGuessed").innerText = letterGuessed.join(" ")
+    }
+}
+
+
+    
